78a97e667f453d02bb4f60e56e06c63210e3d1be
Anik Das <anikdas0010@gmail.com>
2025-08-24 07:56:48 +0530

    Update:
    Updating the Query Sheet with optimized queries

--- retail_sale_db.sql
+++ retail_sale_db.sql
@@ -1,186 +1,209 @@
--- SQL Retail Sales Analysis -- P1
-create database sql_project_p1
-use sql_project_p1
+-- =======================================================================================================================
+--											SQL Retail Sales Analysis Project
+-- =======================================================================================================================
+-- ====================================================================
+--							Data Cleaning
+-- ====================================================================
+use RetailAnalyticsProject
+select 
+	*
+from SalesDB
+where
+	age is null or 
+	category is null or 
+	cogs is null or 
+	customer_id is null or 
+	gender is null or 
+	price_per_unit is null or 
+	quantiy is null or 
+	sale_date is null or 
+	sale_time is null or 
+	total_sale is null or
+	transactions_id  is null
 
--- Creating the Tables
+-- ====================================================================
+--		Updating Null values in Age Column with Avg Age per Gender
+-- ====================================================================
+UPDATE SalesDB
+SET age = sub.avg_age
+FROM SalesDB s
+JOIN (
+    SELECT gender, AVG(age * 1.0) AS avg_age
+    FROM SalesDB
+    WHERE age IS NOT NULL
+    GROUP BY gender
+) sub
+    ON s.gender = sub.gender
+WHERE s.age IS NULL
 
-Create Table retail_sale_db (
-	transactions_id	int primary key,
-	sale_date	date,
-	sale_time	time,
-	customer_id	int,
-	gender	varchar(15),
-	age	int,
-	category varchar(15),	
-	quantiy	int,
-	price_per_unit float,	
-	cogs	float,
-	total_sale float
-)
+-- ====================================================================
+--		Deleting Rows with Null values in Measures Columns
+-- ====================================================================
+DELETE from SalesDB
+where
+	age is null or 
+	category is null or 
+	cogs is null or 
+	customer_id is null or 
+	gender is null or 
+	price_per_unit is null or 
+	quantiy is null or 
+	sale_date is null or 
+	sale_time is null or 
+	total_sale is null or
+	transactions_id  is null
 
--- Finding out the Null Values
+-- ====================================================================
+--							Checking Duplicate Entries
+-- ====================================================================
+SELECT 
+	transactions_id, 
+	COUNT(*) AS count_trnc
+FROM SalesDB
+GROUP BY transactions_id
+HAVING COUNT(*) > 1
 
-select * from retail_sale_db 
-where 
-	transactions_id is null
-	or
-	sale_date is null
-	or
-	sale_time is null
-	or
-	gender is null
-	or 
-	category is null
-	or 
-	quantiy is null
-	or
-	cogs is null
-	or 
-	total_sale is null
-	or 
-	age is null
+-- ====================================================================
+--							Data Exploration
+-- ====================================================================
+-- How many sales we have?
+SELECT 
+	count(*) as TotalSales
+FROM SalesDB
+-- How many uniuque customers we have ?
+SELECT
+	COUNT(DISTINCT customer_id) as NoOfCustomers
+FROM SalesDB
 
+-- =========================================================================
+--	Data Analysis & Business Key Problems & Answers [Analysis & Findings]
+-- =========================================================================
 
--- Data Cleaning (Deleating Null Values)
+-- Q.1 Write a SQL query to retrieve all columns for sales made on '2022-11-05'
+select
+	*
+from SalesDB
+where sale_date = '2022-11-05'
 
-delete from retail_sale_db
-where 
-	transactions_id is null
-	or
-	sale_date is null
-	or
-	sale_time is null
-	or
-	gender is null
-	or 
-	category is null
-	or 
-	quantiy is null
-	or
-	cogs is null
-	or 
-	total_sale is null
-	or
-	age is null
-
--- Renaming the Column name from 'Quantiy' to 'Quantity'
-
-EXEC sp_rename 'retail_sale_db.quantiy', 'Quantity', 'COLUMN';
-
-
--- Data Exploration
-
--- How many Sales we have ?
-
-select count(total_sale) as Total_Sales_count
-from retail_sale_db
-
--- How many unique customers we have ?
-
-select count(distinct customer_id) as [Total Unique Customers]
-from retail_sale_db
-
--- How Many Categories we Have ?
-
-select distinct category
-from retail_sale_db
-
--- Data Analysis and Business Key Problems
-
-
-
--- Q.1 Write a SQL query to retrieve all columns for sales made on '2022-11-05
-
-select * from retail_sale_db
-where sale_date = '2022-11-02'
-
-
--- Q.2 Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is >=4 in the month of Nov-2022
-
-SELECT *
-FROM retail_sale_db
-WHERE category = 'Clothing'
-  AND FORMAT(sale_date, 'yyyy-MM') = '2022-11'
-  AND quantity >= 4;
-
--- Q.3 Write a SQL query to calculate the total sales (total_sale) for each category.
-
-select category, 
-sum(total_sale) [Sale by Category],
-count(*) [Total Order Count]
-from retail_sale_db
-group by category
-
-
--- Q.4 Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
+/*
+Q.2 Write a SQL query to retrieve all transactions where the category is 
+'Clothing' and the quantity sold is more than 3 in the month of Nov-2022
+*/
 
 select 
-round(avg(age), 2) as avg_age
-from retail_sale_db
+	*
+from SalesDB
+where 
+		category = 'Clothing' 
+	and quantiy > 3
+	and MONTH(sale_date) = 11 
+	and year(sale_date) = 2022
+
+-- ====================
+--		Alternatively
+-- ====================
+
+SELECT 
+	*
+FROM SalesDB
+WHERE category = 'Clothing'
+  AND quantiy > 3
+  AND sale_date >= '2022-11-01'
+  AND sale_date < '2022-12-01';
+
+-- Q.3 Write a SQL query to calculate the total sales (total_sale) for each category.
+select
+	category,
+	sum(total_sale) as TotalSales
+from SalesDB
+group by category
+
+-- Q.4 Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
+select
+	category,
+	avg(age) as AvgCustomerAge
+from SalesDB
 where category = 'Beauty'
+group by category
+
+-- ====================
+--		Alternatively
+-- ====================
+
+select
+	category,
+	avg(age) as AvgCustomerAge
+from SalesDB
+group by category
+having category = 'Beauty'
 
 -- Q.5 Write a SQL query to find all transactions where the total_sale is greater than 1000.
-
-select * from retail_sale_db
+select
+	*
+from SalesDB
 where total_sale > 1000
-
 
 -- Q.6 Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.
 
-select category, gender, count(*) [Total Transactions Made]
-from retail_sale_db
-group by gender, category
-order by category
+select
+	gender,
+	category,
+	COUNT(transactions_id) as NoOfTransactions
+from SalesDB
+group by 
+	gender,
+	category
+order by
+	category,
+	NoOfTransactions
 
 -- Q.7 Write a SQL query to calculate the average sale for each month. Find out best selling month in each year
-
-select year, month, round([average Sale], 2) as [average sale] from
+select
+	Year,
+	Month,
+	AvgSales
+from
 (
 select
-	YEAR(sale_date) as year,
-	MONTH(sale_date) as month,
-	avg(total_sale) as [average Sale],
-	rank() over(partition by YEAR(sale_date) order by avg(total_sale) desc) as ranks
-from retail_sale_db
+	YEAR(Sale_date) as Year,
+	month(sale_date) as Month,
+	avg(total_sale) as AvgSales,
+	ROW_NUMBER()OVER(PARTITION BY YEAR(Sale_date) ORDER BY avg(total_sale) desc) as AvgSalesRank
+	from SalesDB
 group by 
-YEAR(sale_date), 
-MONTH(sale_date)) as t1
-where ranks = 1
+	YEAR(Sale_date),
+	month(sale_date)
+)x
+where AvgSalesRank = 1
 
 -- Q.8 Write a SQL query to find the top 5 customers based on the highest total sales 
 
-select top 5
-customer_id, sum(total_sale) as [Total Sales]
-from retail_sale_db
-group by customer_id
-order by sum(total_sale) desc
+select
+	top 5
+	customer_id,
+	sum(total_sale) as TotalSales
+from SalesDB
+group by 
+	customer_id
+order by 
+	sum(total_sale) desc
 
 -- Q.9 Write a SQL query to find the number of unique customers who purchased items from each category.
-
-select  
-category,
-COUNT(distinct customer_id) as [Category Wise Customer Count]
-from retail_sale_db
-group by category
-
+select
+	category,
+	count(distinct customer_id) as UniqueCustomerCount
+from SalesDB
+group by 
+	category
+order by 
+	UniqueCustomerCount desc
 
 -- Q.10 Write a SQL query to create each shift and number of orders (Example Morning <=12, Afternoon Between 12 & 17, Evening >17)
-
-with hourly_sales as
-(
-select *,
-case 
-	when DATEPART(hour, sale_time) < 12 then 'Morning'
-	when DATEPART(hour, sale_time) between 12 and 17 then 'Afternoon'
-	else 'Evening'
-	end as shift
-from retail_sale_db)
-
-select 
-shift,
-count(*) as Total_Orders
-from hourly_sales
-group by shift
-
-
------------------------------------------------------------------------- END ---------------------------------------------------------------------------
+select
+	*,
+	CASE 
+		WHEN DATEPART(hour,sale_time) <= 12 THEN 'Morning'
+		WHEN DATEPART(hour,sale_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+		ELSE 'Evening'
+	END as ShiftTiming
+from SalesDB
